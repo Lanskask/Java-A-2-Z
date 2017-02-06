fe31f5c2a8663b368be60312605ee84d00c6eaf2
Lanskask <alanskaskiy@mail.ru>
2017-02-06 18:37:29 +0000

    Rename TrackerToIdea -> Tracker

--- Part_2_OOP/Chess/src/main/java/ru/smurtazin/chess/game/RunChess.java
+++ Part_2_OOP/Chess/src/main/java/ru/smurtazin/chess/game/RunChess.java
@@ -7,11 +7,12 @@ import ru.smurtazin.chess.parts.ChessDesk;
  */
 public class RunChess {
 
-    ChessDesk chessDesk = new ChessDesk();
-
     public static void main(String[] args) {
         // TODO: 25.01.17   What todo?
         // TODO: 25.01.17   Or nothing?
         // TODO: 25.01.17   Or remove this main method and use just tests?
+
+        ChessDesk chessDesk = new ChessDesk();
+
     }
 }
