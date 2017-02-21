f845ef5bfda558e6e8b7587f95d20ee41ab2e5a9
Lanskask <alanskaskiy@mail.ru>
2017-02-21 21:43:30 +0000

    5.1.3 Iterator of Iterators Duble 1

---
+++ Part_5_Collections/src/main/java/ru/smurtazin/Collections/services/IteratorsIterator.java
@@ -0,0 +1,92 @@
+package ru.smurtazin.Collections.services;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
+/**
+ * Created by a1 on 21.02.17.
+ */
+public class IteratorsIterator implements Iterator {
+
+    // --- Fields -------
+    private int[][] values;
+    private int position = 0; // needed for arrays
+    private int index = 0;
+
+    // --- Constructors -------
+    public IteratorsIterator(int[][] values ) {
+        this.values = values;
+    }
+
+    public IteratorsIterator() {}
+
+    // --- Little needed functions ----------
+    public boolean hasNext() {
+        return values.length > index+1;
+    }
+
+    public Object next() {
+        return values[index++];
+    }
+
+    public void remove() {} // Needed cause of implementation
+
+    // ------ My Realisation ------------
+    private int fullArraySize(int[][] fullArray) {
+        int fullArraySize = 0;
+        for (int[] bigArrayPart : fullArray) {
+            fullArraySize += bigArrayPart.length;
+        }
+        return fullArraySize ;
+    }
+
+    public int[] convertArrays(int[][] fullArray) {
+        int[] iteratorToReturn = new int[this.fullArraySize(fullArray)];
+
+        for (int[] bigArrayPart : fullArray ){
+            for (int num : bigArrayPart  ){
+                iteratorToReturn[position++] = num;
+            }
+        }
+
+        return iteratorToReturn;
+    }
+
+    // --------- For iterators ------------
+    public int size(Iterator iterator) {
+//        return Lists.newArrayList(iterator).size();
+        int i = 0;
+        for ( ; iterator.hasNext() ; ++i ) iterator.next();
+
+        return i + 1;
+    }
+
+    // TODO: Test this
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> iterator) {
+        ListIterator<Integer> iteratorToReturn = new ArrayList<Integer>().listIterator();
+
+        for (int i = 0; i < this.size(iterator); i++) {
+            Iterator<Integer> bigIterPart = iterator.next();
+            for (int j = 0; j < this.size(bigIterPart); j++) {
+                iteratorToReturn.add(bigIterPart.next());
+            }
+        }
+
+        return iteratorToReturn;
+    }
+
+    public Iterator<Integer> convert2(Iterator<Iterator<Integer>> iterator) {
+        ListIterator<Integer> iteratorToReturn = new ArrayList<Integer>().listIterator();
+
+        for (int i = 0; iterator.hasNext(); i++) {
+            Iterator<Integer> bigIterPart = iterator.next();
+            for (int j = 0; bigIterPart.hasNext(); j++) {
+                bigIterPart.next();
+                iteratorToReturn.add(bigIterPart.next());
+            }
+        }
+
+        return iteratorToReturn;
+    }
+}
---
+++ Part_5_Collections/src/test/java/ru/smurtazin/Collections/services/IteratorsIteratorTest.java
@@ -0,0 +1,61 @@
+package ru.smurtazin.Collections.services;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by a1 on 21.02.17.
+ */
+public class IteratorsIteratorTest {
+    int[] intArray1 = {4, 2, 0, 4, 6, 4, 9}; // 7
+    int[] intArray2 = {0, 9, 8, 7, 5}; //5
+    int[] intArray3 = {1, 3, 5, 6, 7, 0, 9, 8, 4}; // 9
+    int[][] endBigArray = {intArray1, intArray2, intArray3}; // 21
+//    IteratorsIterator myIterator = new IteratorsIterator( endBigArray );
+    IteratorsIterator myIterator = new IteratorsIterator();
+
+    /*Iterator<Iterator<Integer>> iterator = (
+        (4, 2, 0, 4, 6, 4, 9),
+        (0, 9, 8, 7, 5),
+        (1, 3, 5, 6, 7, 0, 9, 8, 4)
+    );*/
+
+    Iterator<Integer> iterator1 = new IteratorArray( new int[]{4, 2, 0, 4, 6, 4, 9}); // 7
+    Iterator<Integer> iterator2 = new IteratorArray( new int[]{0, 9, 8, 7, 5}); // 5
+    Iterator<Integer> iterator3 = new IteratorArray( new int[]{1, 3, 5, 6, 7, 0, 9, 8, 4}); // 9
+    // TODO: How to initialize Iterator of Iterators
+//    Iterator<Iterator<Integer>> iteratorsIterator = new IteratorArray(iterator1, iterator2, iterator3);
+
+    @Ignore
+    @Test
+    public void convertArrays() throws Exception {
+        int[] arrayToCheck = {
+                4, 2, 0, 4, 6, 4, 9,
+                0, 9, 8, 7, 5,
+                1, 3, 5, 6, 7, 0, 9, 8, 4
+        };
+        assertThat(arrayToCheck, is(myIterator.convertArrays(endBigArray)));
+    }
+
+    @Test // Works right
+    public void size() throws Exception {
+        assertThat(5, is(myIterator.size(iterator2)));
+    }
+
+    @Ignore
+    @Test
+    public void convert2_Test() throws Exception {
+
+    }
+
+    @Ignore
+    @Test
+    public void convert_Test() throws Exception {
+
+    }
+}
