5f6d56ef397182815730f297d8c462980c50d901
Lanskask <alanskaskiy@mail.ru>
2017-02-25 15:17:59 +0000

    Tests added

---
+++ Part_5_Collections/src/test/java/ru/smurtazin/Collections/services/generics/RoleStoreTest.java
@@ -0,0 +1,39 @@
+package ru.smurtazin.Collections.services.generics;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+/**
+ * Created by a1 on 25.02.17.
+ */
+public class RoleStoreTest {
+
+    RoleStore roleStoreArray = new RoleStore(5);
+    Base role1 = new Role();
+    Base user1 = new User();
+
+    @Test
+    public void add() throws Exception {
+        this.roleStoreArray.add(role1);
+        this.roleStoreArray.add(user1);
+
+        assertThat(this.roleStoreArray.get(0), is(role1));
+    }
+
+    @Test
+    public void update() throws Exception {
+        this.roleStoreArray.update(0, user1);
+
+        assertThat(this.roleStoreArray.get(0), is(user1));
+    }
+
+    @Test
+    public void delete() throws Exception {
+        this.roleStoreArray.delete(0);
+
+        assertNull(this.roleStoreArray.get(0));
+    }
+
+}
