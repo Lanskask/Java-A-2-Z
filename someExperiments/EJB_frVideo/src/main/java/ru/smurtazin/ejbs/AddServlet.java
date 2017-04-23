c6a6ad1493e64c69e8d275762c72c3a3e6ddbadb
Lanskask <alanskaskiy@mail.ru>
2017-04-23 05:40:55 +0000

    Servlet with annotation is working

--- someExperiments/EJB_frVideo/src/main/java/ru/smurtazin/ejbs/AddServlet.java
+++ someExperiments/EJB_frVideo/src/main/java/ru/smurtazin/ejbs/AddServlet.java
@@ -11,8 +11,12 @@ import java.io.PrintWriter;
 /**
  * Created by a1 on 22.04.17.
  */
-@WebServlet(urlPatterns={"/AddServlet"})
+//@WebServlet(urlPatterns={"/AddServlet"})
 public class AddServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doPost(req, resp);
+    }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
