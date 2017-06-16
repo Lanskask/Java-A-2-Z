037d3dd41bdb6239b3b50da665c64b65fc98f786
smurt <murtazin.sergey@fit.ru>
2017-06-16 15:37:49 +0300

    Added spring security from officical guide

--- Part_10_Hibernate_2/src/main/java/ru/smurtazin/services/UserStorage.java
+++ Part_10_Hibernate_2/src/main/java/ru/smurtazin/services/UserStorage.java
@@ -3,6 +3,7 @@ package ru.smurtazin.services;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import ru.smurtazin.hibmodels.Item;
 import ru.smurtazin.hibmodels.User;
 
@@ -14,105 +15,120 @@ import java.util.List;
  * Created by a1 on 24.04.17.
  */
 public class UserStorage {
-	SessionFactory factory = new Configuration()
-			.configure()
-			.buildSessionFactory();
-	Session session = this.factory.openSession();
-	User user = new User();
-	Item item = new Item();
-	List<User> users;
-	List<Item> items;
+    SessionFactory factory = new Configuration()
+            .configure()
+            .buildSessionFactory();
+    Session session = this.factory.openSession();
+    User user = new User();
+    Item item = new Item();
+    List<User> users;
+    List<Item> items;
 
-	public static void main(String[] args) {
-		UserStorage main = new UserStorage();
+    public static void main(String[] args) {
+        UserStorage main = new UserStorage();
 
-		main.run();
-	}
+        main.run();
+    }
 
-	void run() {
-		this.session.beginTransaction();
+    void run() {
+        this.session.beginTransaction();
 
 //        this.create();
 //        this.read();
 //        this.printUsers();
 //        this.update();
 //        this.delete();
-		this.experFunc(); // add Item
-		this.readItems();
-		this.printItems();
+        this.experFunc(); // add Item
+        this.readItems();
+        this.printItems();
 
 //        System.out.println(session.createQuery("from User").list());
-		this.session.getTransaction().commit();
-		this.session.close();
-		this.factory.close();
-	}
+        this.session.getTransaction().commit();
+        this.session.close();
+        this.factory.close();
+    }
 
-	/**
-	 * Just create one this.item
-	 */
-	void experFunc() {
+    /**
+     * Just create one this.item
+     */
+    void experFunc() {
 //		Item item = new Item();
-		this.item.setDesc("Item1 description");
-		this.item.setCreated(new Timestamp(System.currentTimeMillis()));
-		this.item.setAuthor(new User());
-	}
+        this.item.setDesc("Item1 description");
+        this.item.setCreated(new Timestamp(System.currentTimeMillis()));
+        this.item.setAuthor(new User());
+    }
 
-	/**
-	 * Creates or Updates one User - newUser
-	 * Insert newUser into Database
-	 * @param newUser
-	 */
-	void create(User newUser) {
+    /**
+     * Creates or Updates one User - newUser
+     * Insert newUser into Database
+     *
+     * @param newUser
+     */
+    void create(User newUser) {
 //        this.user.setLogin("test"); // TODO: Remove comment - it's old variant
 //        this.user = newUser; // TODO: or:
-		this.user.setId(newUser.getId());
-		this.user.setLogin(newUser.getLogin());
-		this.user.setPassword(newUser.getPassword());
-		this.user.setCreated(newUser.getCreated());
+        this.user.setId(newUser.getId());
+        this.user.setLogin(newUser.getLogin());
+        this.user.setPassword(newUser.getPassword());
+        this.user.setCreated(newUser.getCreated());
 
-		this.session.saveOrUpdate(this.user);
-	}
+        this.session.saveOrUpdate(this.user);
+    }
 
-	/**
-	 * Updates (not create&&||update) one User - newUser
-	 * @param newUser
-	 */
-	void update(User newUser) {
-		this.user.setId(newUser.getId());
-		this.user.setLogin(newUser.getLogin());
-		this.user.setPassword(newUser.getPassword());
-		this.user.setCreated(new Timestamp(System.currentTimeMillis()));
+    /**
+     * Updates (not create&&||update) one User - newUser
+     *
+     * @param newUser
+     */
+    void update(User newUser) {
+        this.user.setId(newUser.getId());
+        this.user.setLogin(newUser.getLogin());
+        this.user.setPassword(newUser.getPassword());
+        this.user.setCreated(new Timestamp(System.currentTimeMillis()));
 
-		this.session.update(this.user);
-	}
+        this.session.update(this.user);
+    }
 
-	/**
-	 * Delete one user
-	 * Check through: assertThat(userStorage.users, is(arrayToCompareWith.remove(User)) );
-	 * @param newUser
-	 */
-	void delete(User newUser) {
-		this.user.setId(newUser.getId());
+    /**
+     * Delete one user
+     * Check through: assertThat(userStorage.users, is(arrayToCompareWith.remove(User)) );
+     *
+     * @param newUser
+     */
+    void delete(User newUser) {
+        this.user.setId(newUser.getId());
 
-		this.session.delete(this.user);
-	}
+        this.session.delete(this.user);
+    }
 
-	/**
-	 * List of all Users.
-	 * get throw: UsesrStorage.users;
-	 */
-	void readUsers() {
-		this.users = this.session.createQuery("from User").list();
-	}
+    /**
+     * List of all Users.
+     * get throw: UsesrStorage.users;
+     */
+    void readUsers() {
+        this.users = this.session.createQuery("from User").list();
+    }
 
-	User findUserById(int id) {
-//		return this.session.createQuery("from User where id = ?");
+    User findUserById(int id) {
+//		return this.session.createQuery("from User where id = :id").setParameter("id", id).list().get(0);
 
+        String hql = "FROM User WHERE id = :id";
+        Query query = this.session.createQuery(hql);
+        query.setParameter("id", id);
+        List<User> userList = query.list();
+        User user;
 
-	}
+        try {
+            user = userList.get(0);
+            return user;
+        } catch (Exception e) {
+            System.out.println("error");
+            return null;
+        }
+    }
 
 /*	public User getBusById(Long bus_id) throws SQLException {
-		Session session = null;
+        Session session = null;
 		Bus bus = null;
 		try {
 			session = HibernateUtil.getSessionFactory().openSession();
@@ -126,30 +142,30 @@ public class UserStorage {
 		return bus;
 	}*/
 
-	/**
-	 * List of all Items.
-	 * get through: UsesrStorage.items;
-	 */
-	void readItems() {
-		this.items = this.session.createQuery("from Item").list();
-	}
+    /**
+     * List of all Items.
+     * get through: UsesrStorage.items;
+     */
+    void readItems() {
+        this.items = this.session.createQuery("from Item").list();
+    }
 
-	/**
-	 * Print Users logins
-	 */
-	void printUsers() {
-		for (User user : this.users) {
-			System.out.println(user.getLogin());
-		}
-	}
+    /**
+     * Print Users logins
+     */
+    void printUsers() {
+        for (User user : this.users) {
+            System.out.println(user.getLogin());
+        }
+    }
 
-	/**
-	 * Print Items.toString
-	 */
-	void printItems() {
-		for (Item item : this.items) {
-			System.out.println(item.toString());
-		}
-	}
+    /**
+     * Print Items.toString
+     */
+    void printItems() {
+        for (Item item : this.items) {
+            System.out.println(item.toString());
+        }
+    }
 
 }
