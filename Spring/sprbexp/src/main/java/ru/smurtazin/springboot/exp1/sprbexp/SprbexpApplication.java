62d5069ea0a04c7195e756b3967ecc76fcdc7b53
a1 <alanskaskiy@mail.ru>
2017-11-27 19:52:23 +0300

    Experimenting with Tomcat Spring Annotations and SprBoot Unversioned

---
+++ Spring/sprbexp/src/main/java/ru/smurtazin/springboot/exp1/sprbexp/SprbexpApplication.java
@@ -0,0 +1,12 @@
+package ru.smurtazin.springboot.exp1.sprbexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprbexpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprbexpApplication.class, args);
+	}
+}
---
+++ Spring/sprbexp/src/test/java/ru/smurtazin/springboot/exp1/sprbexp/SprbexpApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.smurtazin.springboot.exp1.sprbexp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprbexpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
