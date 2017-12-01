7730b5cd18a518f416c4ed407209640c343b8cb3
Lanskask <alanskaskiy@mail.ru>
2017-12-01 11:14:07 +0900

    Add exp with Thym Login from database

---
+++ SpringSecurityJdbcApp-master/mav_auth.sql
@@ -0,0 +1,72 @@
+drop table IF EXISTS users CASCADE;
+drop TABLE IF EXISTS user_roles CASCADE;
+drop SEQUENCE IF EXISTS user_roles_seq;
+
+CREATE  TABLE users (
+  username VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL,
+  enabled BOOLEAN NOT NULL DEFAULT TRUE,
+  PRIMARY KEY (username)
+);
+CREATE SEQUENCE user_roles_seq;
+
+CREATE TABLE user_roles (
+  user_role_id int NOT NULL DEFAULT NEXTVAL ('user_roles_seq'),
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+  PRIMARY KEY (user_role_id),
+  CONSTRAINT uni_username_role UNIQUE (role,username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
+);
+
+CREATE INDEX fk_username_idx ON user_roles (username);
+INSERT INTO users(username,password,enabled)
+  VALUES ('priya','priya', true);
+INSERT INTO users(username,password,enabled)
+  VALUES ('naveen','naveen', true);
+INSERT INTO user_roles (username, role)
+  VALUES ('priya', 'ROLE_USER');
+INSERT INTO user_roles (username, role)
+  VALUES ('priya', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role)
+  VALUES ('naveen', 'ROLE_USER');
+/*
+CREATE  TABLE users (
+id SERIAL NOT NULL PRIMARY KEY,
+username VARCHAR(45) NOT NULL,
+password VARCHAR(45) NOT NULL,
+enabled BOOLEAN NOT NULL DEFAULT TRUE
+);
+CREATE SEQUENCE user_roles_seq;
+CREATE TABLE user_roles (
+  user_role_id INTEGER NOT NULL PRIMARY KEY default nextval ('user_roles_seq'),
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+
+  CONSTRAINT uni_username_role UNIQUE (role, username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
+);
+CREATE INDEX fk_username_idx ON user_roles (username);
+*/
+
+/*CREATE TABLE user_roles (
+  user_role_id serial NOT NULL PRIMARY KEY,
+  username varchar(45) NOT NULL,
+  role varchar(45) NOT NULL,
+
+  UNIQUE KEY uni_username_role (role, username),
+  KEY fk_username_idx (username),
+  CONSTRAINT fk_username FOREIGN KEY (username) REFERENCES users (username)
+);*/
+
+/*
+INSERT INTO users(username,password,enabled)
+  VALUES ('priya','priya', true);
+INSERT INTO users(username,password,enabled)
+  VALUES ('naveen','naveen', true);
+INSERT INTO user_roles (username, role)
+  VALUES ('priya', 'ROLE_USER');
+INSERT INTO user_roles (username, role)
+  VALUES ('priya', 'ROLE_ADMIN');
+INSERT INTO user_roles (username, role)
+  VALUES ('naveen', 'ROLE_USER');*/
