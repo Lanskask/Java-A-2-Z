2feb00e9d0bc025ce13b7a8cf4064a1225b2bf78
Lanskask <alanskaskiy@mail.ru>
2017-12-01 19:44:54 +0900

    Little changings

--- SpringSecurityJdbcApp-master/src/main/java/hello/WebSecurityConfig.java
+++ SpringSecurityJdbcApp-master/src/main/java/hello/WebSecurityConfig.java
@@ -28,7 +28,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-
 	  http.authorizeRequests()
 		.antMatchers("/hello").access("hasRole('ROLE_ADMIN')")		
 		.anyRequest().permitAll()
